3406f01248390947ca98d26ec16d821062671d99
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-10-17 22:38:59 +0200

    a game starts with player 1 turn

--- test/pigs/core_test.clj
+++ test/pigs/core_test.clj
@@ -3,11 +3,16 @@
             [pigs.core :refer :all]))
 
 (defn pigs [players]
-  {:scores (repeat players 0)})
+  {
+   :scores (repeat players 0)
+   :player-turn 1
+   })
 
 (deftest pig-game-creation-test
   (testing "a game starts with the number of players passed in"
     (is (= 2 (count (:scores (pigs 2)))))
     (is (= 3 (count (:scores (pigs 3))))))
   (testing "all scores are zero when the game starts"
-    (is (every? zero? (:scores (pigs 7))))))
+    (is (every? zero? (:scores (pigs 7)))))
+  (testing "a game starts with player 1 turn"
+    (is (= 1 (:player-turn (pigs 8))))))
