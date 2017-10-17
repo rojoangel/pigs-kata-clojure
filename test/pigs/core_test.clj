d050f1d837e0f78326db0ecaefdf7b1041db3e69
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-10-17 22:31:02 +0200

    refine a game starts with the number of players passed in

--- test/pigs/core_test.clj
+++ test/pigs/core_test.clj
@@ -3,8 +3,9 @@
             [pigs.core :refer :all]))
 
 (defn pigs [players]
-  {:scores [0 0]})
+  {:scores (repeat players 0)})
 
 (deftest pig-game-creation-test
   (testing "a game starts with the number of players passed in"
-    (is (= 2 (count (:scores (pigs 2)))))))
+    (is (= 2 (count (:scores (pigs 2)))))
+    (is (= 3 (count (:scores (pigs 3)))))))
