6b59a00a65551652585c6bd234f98948130041ba
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-10-21 11:51:07 +0200

    shows game state

--- src/pigs/command_line.clj
+++ src/pigs/command_line.clj
@@ -12,9 +12,15 @@
     (let [[players-arg & _] (str/split (read-line) #" ")]
       (read-string players-arg))))
 
+(defn- show-game-state [game-state]
+  (let [scores (str/join ", " (:scores game-state))
+        player-turn (str "player " (:player-turn game-state))
+        turn (str/join ", " (:current-player-rolls game-state))]
+    (println "scores: [" scores "] " player-turn " turn: [" turn "]")))
+
 (defn -main [& args]
   (do
     (salute)
     (-> (read-number-of-players)
         (pigs/new-game)
-        )))
+        (show-game-state))))
